8c28b6c91fbb9c5306f0f05e0c8d4a068314294b
Z0R7N <dmtrii.a@gmail.com>
2021-05-16 06:23:50 +0300

    update path of picture

--- src/test/java/steps/TaskThreeStep.java
+++ src/test/java/steps/TaskThreeStep.java
@@ -2,9 +2,13 @@ package steps;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 public class TaskThreeStep extends BaseTest{
     @Test
     public void threeTest(){
+        File file = new File("src/main/resources/picTest.png");
+
         main.goTo().chooseTask("Zadanie 3");
         taskThreePage
                 .mouseOver("//a[text()[contains(.,'Menu')]]")
@@ -12,9 +16,9 @@ public class TaskThreeStep extends BaseTest{
                 .mouseOver("//a[text()[contains(.,'do trybu')]]")
                 .inputText("in-name", "Tim")
                 .inputText("in-surname", "Andersen")
-                .inputText("in-notes", "sdlkf fsdgkll tw;gwwp4 rfw5 jkwjkgsd lfsdjgwq45qjg kj")
+                .inputText("in-notes", "Bacon ipsum dolor amet pork ground round cow andouille pastrami spare ribs ham hock bacon drumstick pork loin fatback.")
                 .inputText("in-phone", "54654634")
-                .uploadPicture("//input[@id='in-file']", "D:\\CodeProjects\\ASTA_3\\src\\assert\\picTest.png")
+                .uploadPicture("//input[@id='in-file']", file.getAbsolutePath())
                 .clickButton("save-btn");
     }
 }
